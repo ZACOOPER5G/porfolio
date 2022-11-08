dd68bc7b3db0530a8db9a86b876cca07bf1aa7e1
Zac Cooper <zacooper5g@outlook.com>
2022-11-08 14:44:49 +1100

    updated skills and how they are displayed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,7 +10,8 @@ import node from '../assets/img/node.png';
 import bootstrap from '../assets/img/bootstrap.svg';
 import git from '../assets/img/git-logo.png';
 import ts from '../assets/img/ts.png';
-import redux from '../assets/img/redux.svg';
+import express from '../assets/img/express.svg';
+import mongo from '../assets/img/mongo.png';
 
 export const Skills = () => {
     const responsive = {
@@ -42,41 +43,45 @@ export const Skills = () => {
                             Skills
                         </h2>
                         <p>Here are some of my favourite technologies, and the ones I have been working with most recently:</p>
-                        <Carousel responsive={responsive} infinite={true} className="skill-slider" autoPlay={true} autoPlaySpeed={1800} >
+                        <Carousel responsive={responsive} infinite={true} className="skill-slider" autoPlay={true} autoPlaySpeed={1800} itemClass="image-item">
                             <div className="item">
-                                <img src={reactIMG} alt='Image' />
+                                <img src={reactIMG} alt='skill-logo' />
                                 <h5>React</h5>
                             </div>
                             <div className="item">
-                                <img src={js} alt='Image' />
+                                <img src={js} alt='skill-logo' />
                                 <h5>JavaScript</h5>
                             </div>
                             <div className="item">
-                                <img src={ts} alt='Image' />
+                                <img src={ts} alt='skill-logo' />
                                 <h5>TypeScript</h5>
                             </div>
                             <div className="item">
-                                <img src={html} alt='Image' />
+                                <img src={html} alt='skill-logo' />
                                 <h5>HTML</h5>
                             </div>
                             <div className="item">
-                                <img src={css} alt='Image' />
+                                <img src={css} alt='skill-logo' />
                                 <h5>CSS</h5>
                             </div>
                             <div className="item">
-                                <img src={node} alt='Image' />
-                                <h5>Node JS</h5>
+                                <img src={node} alt='skill-logo' />
+                                <h5>Node.js</h5>
+                            </div>
+                            <div className="item express">
+                                <img src={express} alt='skill-logo' />
+                                <h5>Express.js</h5>
                             </div>
                             <div className="item">
-                                <img src={redux} alt='Image' />
-                                <h5>Redux</h5>
+                                <img src={mongo} alt='skill-logo' />
+                                <h5>MongoDB</h5>
                             </div>
                             <div className="item">
-                                <img src={bootstrap} alt='Image' />
+                                <img src={bootstrap} alt='skill-logo' />
                                 <h5>Bootstrap</h5>
                             </div>
                             <div className="item">
-                                <img src={git} alt='Image' />
+                                <img src={git} alt='skill-logo' />
                                 <h5>Git</h5>
                             </div>
                         </Carousel>
