f8dbc3ec32374b8b992025896096ae9f62604b53
Zac Cooper <zacooper5g@outlook.com>
2022-11-21 08:16:16 +1100

    rolled back changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -22,15 +22,15 @@ export const Skills = () => {
         },
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
-          items: 4
+          items: 3
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 3
+          items: 2
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
-          items: 2
+          items: 1
         }
       };
 
